16ebd63a63aa382210bb9679d2ad7fb3994be141
nguyentrongbut <cloly031128@gmail.com>
2026-03-06 17:54:00 +0700

    feat(sitemap): add detail page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,28 @@
-export default async function sitemap() {
-    const baseUrl = process.env.NEXT_PUBLIC_YOUR_WEBSITE;
+// ** Next
+import { MetadataRoute } from "next"
+
+// ** Mock data
+import { mockDataDetail } from "@/mock-data"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = process.env.NEXT_PUBLIC_YOUR_WEBSITE || "http://localhost:3000"
+
+    const routeDetail = "project"
+
+    const detailUrl = mockDataDetail.map((detail) => ({
+        url: `${baseUrl}/${routeDetail}/${detail.slug}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly" as const,
+        priority: 0.9,
+    }))
 
     return [
         {
             url: baseUrl,
             lastModified: new Date(),
-            changeFrequency: 'daily',
-            priority: 1.0,
-        }
+            changeFrequency: "daily",
+            priority: 1,
+        },
+        ...detailUrl,
     ]
 }
